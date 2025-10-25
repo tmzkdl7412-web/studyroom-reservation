504a968c0fa32a9013509a9e9ff0341a6605543e
tmzkdl7412-web <tmzkdl7412@gmail.com>
2025-10-25 15:55:40 +0900

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -5,12 +5,11 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    # ✅ 현재 디렉토리 기준으로 templates 경로 지정
+    app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), '../templates'))
 
-    # ✅ 환경변수에서 DB URL 불러오기
     database_url = os.getenv("DATABASE_URL")
     if database_url and database_url.startswith("postgres://"):
-        # 🩵 Railway는 postgres://로 시작하지만, SQLAlchemy는 postgresql://를 요구함
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
@@ -19,7 +18,6 @@ def create_app():
 
     db.init_app(app)
 
-    # ✅ 테이블 생성
     with app.app_context():
         from db import models
         db.create_all()
